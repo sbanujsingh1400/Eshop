8921cf5da4c09289582b3a83f894567c7ef3fd44
sbanujsingh1400 <87994121+sbanujsingh1400@users.noreply.github.com>
2025-11-08 22:12:16 +0530

    become a sellor changes attempt 3

--- apps/user-ui/src/app/configs/constants.tsx
+++ apps/user-ui/src/app/configs/constants.tsx
@@ -13,9 +13,7 @@ href:'/offers'
 },
 {title:"Become A Seller",
 href:  `${
-    process.env.NEXT_PUBLIC_NODE_ENVIRONMENT === 'production'
-      ? process.env.NEXT_PUBLIC_SELLER_URI + '/signup'
-      : process.env.NEXT_PUBLIC_SELLER_URI_LOCAL + '/signup'
+    process.env.NEXT_PUBLIC_SELLER_URI?.includes('eshop')?"https://eshop.seller.mypersonalproject.site/signup":"https://localhost:3000/signup"
   }`
 }
 
