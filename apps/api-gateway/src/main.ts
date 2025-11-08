1e390e891a6ac712d9ad5735934c0229584dfd31
Anuj Singh <sbanujsingh@gmail.com>
2025-11-08 18:12:33 +0530

    new changed workfloe15

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -14,7 +14,7 @@ import initializeConfig from './libs/InitializeSiteConfig';
 const app = express();
 const user_uri =process.env.NEXT_PUBLIC_SELLER_URI!
 const seller_uri =process.env.NEXT_PUBLIC_USER_URI!
-console.log("_________",user_uri,seller_uri,"_________changed again__")
+console.log("_________",user_uri,seller_uri,"_________changed__")
 //Middlewares declaration
 console.log(user_uri,seller_uri)
  app.use(cors({
